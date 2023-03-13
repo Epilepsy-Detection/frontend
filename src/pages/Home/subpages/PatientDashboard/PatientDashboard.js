75b2cc7fc4f8eb50448227f83675bcf4e456fa61
Yosra Emad <yosraemad14@gmail.com>
2023-03-13 21:32:17 +0200

    created dropdown menu

--- src/pages/Home/subpages/PatientDashboard/PatientDashboard.js
+++ src/pages/Home/subpages/PatientDashboard/PatientDashboard.js
@@ -4,6 +4,10 @@ import { BsPersonFill } from "react-icons/bs";
 import ReportsTable from "./components/ReportsTable";
 
 const PatientDashboard = () => {
+  const showDropdown = () => {
+    const dropdown = document.querySelector(`.${styles.dropdown}`);
+    dropdown.classList.toggle(styles["hide"]);
+  };
   return (
     <div className={styles.background}>
       <header className={styles.header}>
@@ -11,8 +15,13 @@ const PatientDashboard = () => {
           <img alt="Logo" src={logo}></img>
           <h1>Hello, Ahmed</h1>
         </div>
-        <div className={styles.profile}>
+        <div className={styles.profile} onClick={showDropdown}>
           <BsPersonFill size={40} color="#fea8db" />
+        </div>
+        <div className={styles.dropdown + " " + styles.hide}>
+          <a href="#">Edit Profile</a>
+          <a href="#">Emergency Contacts</a>
+          <a href="#">Logout</a>
         </div>
       </header>
       <main className={styles["reports-table"]}>
