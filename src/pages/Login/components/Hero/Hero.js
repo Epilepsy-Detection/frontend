d8704d230ddc72c218fe2f8c969351ef7c2f2bba
Yosra Emad <yosraemad14@gmail.com>
2023-04-02 03:34:11 +0200

    Remove logo text from login and sign up

--- src/pages/Login/components/Hero/Hero.js
+++ src/pages/Login/components/Hero/Hero.js
@@ -3,8 +3,7 @@ import styles from "./Hero.module.css";
 const Hero = () => {
   return (
     <section className={styles.hero}>
-      <h1>Logo</h1>
-      <img src={hero} alt="Hero"></img>
+      <img className={styles["hero-img"]} src={hero} alt="Hero"></img>
     </section>
   );
 };
